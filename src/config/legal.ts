e26dfd8832045fc65881a739d675f705a07e97a6
jihyeon <jh0neee@naver.com>
2026-09-05 02:08:55 +0900

    [FIX] 약관 링크 교체

--- src/config/legal.ts
+++ src/config/legal.ts
@@ -6,7 +6,7 @@ export const PRIVACY_POLICY_URL =
 // 이용약관 문서가 확정되면 공개 URL을 넣습니다. 백엔드 연동 뒤에는 로그인
 // 응답의 현재 약관 URL과 버전을 기준으로 대체할 예정입니다.
 export const TERMS_OF_SERVICE_URL =
-  'https://app.notion.com/p/3d04e03f580580fd915ee9cf107483b4';
+  'https://narrow-currant-57e.notion.site/3d04e03f580580fd915ee9cf107483b4';
 
 /** 현재 서비스 이용약관 최신 버전 (GET /terms/service 기준 기본값) */
 export const CURRENT_TERMS_VERSION =
